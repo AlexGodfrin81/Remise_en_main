2a81ec0ecc471fe301bee1910c9705c8f0d1ea98
Alex <Neptuna1998@gmail.com>
2020-11-09 11:01:05 +0100

    Création d'un git pour se remettre les commandes Netbeans en main

---
+++ src/main/java/com/mycompany/gittest/Inversor.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.gittest;
+
+/**
+ *
+ * @author ALEX
+ */
+public class Inversor {
+    
+    public Inversor(){}
+    
+    public String inverse(String str){
+        String newStr = "";
+        for (int i=str.length()-1;i>=0;i--){
+            newStr += str.charAt(i);
+        }
+        return newStr;
+    }
+    
+    public static void main(String[] args){
+        Inversor i = new Inversor();
+        System.out.println(i.inverse("Alex"));
+    }
+    
+}
